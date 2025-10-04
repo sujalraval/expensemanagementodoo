ac28c07a95fffdf846e67dd612bf7b8f81ea516a
harshmistry3000 <harshmistry3000@gmail.com>
2025-10-04 14:36:07 +0530

    project setup completed

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello, Express.js with procjet start ");
+});
+
+// Use PORT from .env (fallback to 3000)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`DB URI: ${process.env.DB_URI}`); // just to check env working
+});
